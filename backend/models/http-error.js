d4ee49635fd8e3ba001760ab6682b33e06521bda
michaelhunter555 <michael.hunter22620@gmail.com>
2026-02-21 20:41:55 -0500

    decouple backend

--- backend/models/http-error.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error {
-  constructor(message, error) {
-    super(message); // message property
-    this.code = error; // code property
-  }
-}
-
-module.exports = HttpError;
